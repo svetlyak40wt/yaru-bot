645da58a6eb126856de3b23169b6a04364fd5f54
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-09-13 16:01:27 +0400

    Сделан запуск бота, как демона, через supervisord.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'yaru-bot',
-    version = '0.1.0',
+    version = 'dev',
     description = 'Jabber bot for http://wow.ya.ru/',
     author = 'Alexander Artemenko',
     author_email = 'svetlyak.40wt@gmail.com',
